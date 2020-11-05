9606f5f34036217162a014a11f8f993f4a1c715e
Denis <47863172+Verakhab@users.noreply.github.com>
2020-11-05 19:04:21 +0300

    corrected cors

--- app.js
+++ app.js
@@ -13,19 +13,24 @@ const { PORT } = process.env;
 
 const app = express();
 
-const allowedCors = (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', [
-    'http://localhost:8080/',
-    'http://web.students.nomoreparties.co/',
-    'https://web.students.nomoreparties.co/',
-    'http://www.web.students.nomoreparties.co/',
-    'https://www.web.students.nomoreparties.co/',
-    'https://verakhab.github.io/news-explorer-frontend/',
-  ]);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
+const allowedCors = [
+  'http://localhost:8080/',
+  'http://web.students.nomoreparties.co/',
+  'https://web.students.nomoreparties.co/',
+  'http://www.web.students.nomoreparties.co/',
+  'https://www.web.students.nomoreparties.co/',
+  'https://verakhab.github.io/news-explorer-frontend/',
+];
+
+app.use((req, res, next) => {
+  const { origin } = req.headers;
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+  }
   next();
-};
+});
 
 app.use(allowedCors);
 
