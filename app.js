3011ec3c24296578193785947e1476f459736563
Denis <47863172+Verakhab@users.noreply.github.com>
2020-11-05 18:45:49 +0300

    corrected cors

--- app.js
+++ app.js
@@ -13,6 +13,22 @@ const { PORT } = process.env;
 
 const app = express();
 
+const allowedCors = (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', [
+    'http://localhost:8080/',
+    'http://web.students.nomoreparties.co/',
+    'https://web.students.nomoreparties.co/',
+    'http://www.web.students.nomoreparties.co/',
+    'https://www.web.students.nomoreparties.co/',
+    'https://verakhab.github.io/news-explorer-frontend/',
+  ]);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+};
+
+app.use(allowedCors);
+
 app.set('trust proxy');
 
 app.use(helmet());
