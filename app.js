82587284ee352f9b2ca9884b24073a97258495fb
Denis <47863172+Verakhab@users.noreply.github.com>
2020-11-06 13:56:54 +0300

    fix cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const helmet = require('helmet');
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
@@ -12,6 +13,8 @@ const errorHandler = require('./middlewares/error-handler');
 const { PORT } = process.env;
 
 const app = express();
+
+app.use(cors());
 
 app.set('trust proxy');
 
